f793704491c6a598d2a29237170e1e397b4cb30e
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-12-01 16:25:13 +0800

    refactored code so that it exports an object with the functions instead

--- views/localGame.js
+++ views/localGame.js
@@ -1,17 +1,28 @@
 import { goBackToMainMenu } from "../script.js";
 import gameUtils from "../gameUtils.js";
 
-const gameDiv = document.querySelector(".game");
+export default (() => {
+    let player1, player2;
 
-export default function displayLocalGame(player1, player2) {
+    function display(p1, p2) {
+        player1 = p1;
+        player2 = p2;
 
-    if (player1 === "") player1 = "Player 1";
-    if (player2 === "") player2 = "Player 2";
+        if (player1 === "") player1 = "Player 1";
+        if (player2 === "") player2 = "Player 2";
 
-    const PlayerUtils = (function () {
-        let currentPlayer = player1;
+        const gameDiv = document.querySelector(".game");
+        gameDiv.append(PlayerUtils.createCurrentPlayerText());
+        gameDiv.append(GameBoard.create());
+        gameDiv.append(QuitButton.create());
+    }
+
+    const PlayerUtils = (() => {
+        let currentPlayer;
 
         function createCurrentPlayerText() {
+            currentPlayer = player1;
+
             const currentPlayerText = document.createElement("p");
             currentPlayerText.id = "current-player";
             currentPlayerText.textContent = `It's ${currentPlayer}'s turn`;
@@ -28,9 +39,21 @@ export default function displayLocalGame(player1, player2) {
         return { createCurrentPlayerText, switchCurrentPlayer };
     })();
 
-    function createGameBoard() {
-
+    const GameBoard = (() => {
         let currentSymbol = "X";
+
+        function assignSymbolAndCheckGame() {
+            if (this.innerText === "") {
+                this.innerText = currentSymbol;
+                if (gameUtils.gameEnded()) {
+                    const winner = gameUtils.getWinner();
+                    console.log(winner);
+                } else {
+                    currentSymbol = currentSymbol === "X" ? "O" : "X";
+                    PlayerUtils.switchCurrentPlayer();
+                }
+            }
+        }
 
         function generateSpace(parentBoard) {
             for (let i = 1; i <= 9; i++) {
@@ -49,37 +72,31 @@ export default function displayLocalGame(player1, player2) {
             }
         }
 
-        function assignSymbolAndCheckGame() {
-            if (this.innerText === "") {
-                this.innerText = currentSymbol;
-                if (gameUtils.gameEnded()) {
-                    const winner = gameUtils.getWinner();
-                    console.log(winner);
-                } else {
-                    currentSymbol = currentSymbol === "X" ? "O" : "X";
-                    PlayerUtils.switchCurrentPlayer();
-                }
-            }
+        function create() {
+            const gameBoard = document.createElement("div");
+            gameBoard.classList.add("gameboard");
+            generateSpace(gameBoard);
+
+            return gameBoard;
         }
 
-        const gameBoard = document.createElement("div");
-        gameBoard.classList.add("gameboard");
-        generateSpace(gameBoard);
+        return { create };
+    })();
 
-        return gameBoard;
-    }
+    const QuitButton = (() => {
 
-    function createQuitButton() {
-        const quitButton = document.createElement("button");
-        quitButton.innerText = "Quit";
-        quitButton.classList.add("quit");
-        quitButton.onclick = goBackToMainMenu;
+        function create() {
+            const quitButton = document.createElement("button");
+            quitButton.innerText = "Quit";
+            quitButton.classList.add("quit");
+            quitButton.onclick = goBackToMainMenu;
 
-        return quitButton;
-    }
+            return quitButton;
+        }
 
+        return { create };
+    })();
 
-    gameDiv.append(PlayerUtils.createCurrentPlayerText());
-    gameDiv.append(createGameBoard());
-    gameDiv.append(createQuitButton());
-}
+    // These are the only usable properties and methods outside this file
+    return { display, PlayerUtils }
+})();
