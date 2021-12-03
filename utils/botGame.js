9d4c2c721e665b801d4fa1bfdd2c8bcaab0c4433
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-12-03 10:52:04 +0800

    just initialized the game mode difficulties
    the logic isn't there yet

---
+++ utils/botGame.js
@@ -0,0 +1,21 @@
+export default (() => {
+
+    function easyMode() {
+        return;
+    }
+
+    function normalMode() {
+        return;
+    }
+
+    function hardMode() {
+        return;
+    }
+
+    return {
+        easyMode, 
+        normalMode, 
+        hardMode
+    }
+
+})();
