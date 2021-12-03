b57a9ed565520026cbb32d2866cf448e5515adc0
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-12-03 10:50:34 +0800

    added the logic for local game behavior

---
+++ utils/localGame.js
@@ -0,0 +1,21 @@
+import Game from "../views/game.js"
+import GameLogic from "./gameLogic.js"
+import GameResults from "../views/gameResults.js"
+
+export default (() => {
+    function behavior() {
+        if (this.innerText === "") {
+            this.innerText = Game.GameBoard.getCurrentSymbol;
+            if (GameLogic.gameEnded()) {
+                Game.GameBoard.disableSpaces();
+                const winner = GameLogic.getWinner();
+                GameResults.display(winner);
+            } else {
+                Game.GameBoard.switchCurrentSymbol();
+                Game.PlayerUtils.switchCurrentPlayer();
+            }
+        }
+    }
+
+    return { behavior }
+})();
